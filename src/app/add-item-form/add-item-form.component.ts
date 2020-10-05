9fdba3d32081de5506eb89b89b6fd8762ae0beeb
anst9000 <coltlaboy@yahoo.se>
2020-10-05 09:47:52 +0200

    first commit

---
+++ src/app/add-item-form/add-item-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
+
+@Component({
+  selector: 'app-add-item-form',
+  templateUrl: './add-item-form.component.html',
+  styleUrls: ['./add-item-form.component.scss'],
+})
+export class AddItemFormComponent implements OnInit {
+  @Input() item: BudgetItem = new BudgetItem('', 0);
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
+}
